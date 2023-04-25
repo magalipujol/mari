e153ec7c58625a7df3516c3d8b66c8bd015c72a8
magalicpujol <magalicpujol@gmail.com>
2023-04-25 13:43:17 +0100

    agrega event listeners para blur y focus
    Signed-off-by: magalicpujol <magalicpujol@gmail.com>

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-var nameInput = document.getElementById('name');
-var passwordInput = document.getElementById('password');
 
+var nameInput = document.getElementById('name');
+var nameError = document.getElementById('nameError');
+var passwordInput = document.getElementById('password');
+var passwordError = document.getElementById('passwordError');
+
+
+
+nameInput.addEventListener("focus", (event) => {
+    event.target.style.background = "pink";
+});
+nameInput.addEventListener("blur", (event) => {
+    event.target.style.background = "";
+    validateName();
+});
+
+password.addEventListener("focus", (event) => {
+    event.target.style.background = "pink";
+});
+password.addEventListener("blur", (event) => {
+    event.target.style.background = "";
+    validatePassword();
+});
 
 function validateName() {
     var name = nameInput.value;
@@ -16,7 +36,9 @@ function validatePassword() {
         alert('Password must be at least 8 characters long');
     }
     if (password.search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) < 0) {
-        alert('Password must contain a number');
+        var message = 'Password must contain a number'
+        passwordError.innerHTML = message;
+        alert(message);
     }
 
     return false;
